012ac69bdcfa5e67076cd9ff74a69a113942a315
eon0525 <kkgoatmxj@gmail.com>
2024-10-30 21:59:29 +0900

    test page

---
+++ Booking_System/src/booking_page/book01.java
@@ -0,0 +1,9 @@
+package booking_page;
+
+public class book01 {
+	    public static void main(String[] args) {
+	        System.out.println("비행기 예약 프로그램");
+	        System.out.println("일본 가고 싶다");
+	}
+}
+ 
